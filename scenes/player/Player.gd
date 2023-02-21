3aee52f0f201a48a33832d6281f09699a8927c00
Thomas Heinrichs <tom.heinri@gmail.com>
2023-02-20 19:59:09 -0500

    8 dir movement implemented with keys

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 var speed = 400
-var animation_dir
-var target
-var path
-var input_vector = Vector2.ZERO
+var input_vector:Vector2 = Vector2.ZERO
+var past_vector:Vector2
+
 
 func _ready():
-	$AnimationPlayer.play("idle")
+	$AnimationTree.get("parameters/playback").travel("idle-loop")
 
 
 func get_input():
@@ -15,24 +14,27 @@ func get_input():
 	input_vector.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	input_vector = input_vector.normalized()
 
-func _physics_process(_delta):
+
+func update_anim_params(move_vector:Vector2):
+	$AnimationTree.set("parameters/Movement/blend_position", input_vector)
+
+
+func _physics_process(delta):
+	$"AnimationTree".advance(delta)
 	get_input()
 	
-	prints("anim_dir", input_vector)
 	if input_vector == Vector2.ZERO:
-		print("IDLE")
-		$AnimationTree.get("parameters/playback").travel("Idle")
+		$AnimationTree.get("parameters/playback").travel("idle-loop")
 	else:
-		print("MOVE")
 		$AnimationTree.get("parameters/playback").travel("Movement")
-		velocity = input_vector * speed
-		move_and_slide()
-		$AnimationTree.set("parameters/Idle/blend_position", input_vector)
-		$AnimationTree.set("parameters/Walk/blend_position", input_vector)
+	
+	update_anim_params(input_vector)
+	velocity = input_vector * speed
+	move_and_slide()
 	
 	if Input.is_action_pressed("player_left") || Input.is_action_pressed("player_right") || Input.is_action_pressed("player_up") || Input.is_action_pressed("player_down"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
 	
+	past_vector = input_vector
--- scenes/world/Space.gd
+++ scenes/world/Space.gd
@@ -1,13 +1,2 @@
 extends Node2D
 
-var target = Vector2()
-signal click_on_space(target)
-
-func _ready():
-	connect("click_on_space", Actions._on_click_on_space)
-
-func _unhandled_input(event):
-	if event.is_action_pressed('click'):
-		if References.dialog.in_progress == false:
-			target = get_global_mouse_position()
-			emit_signal("click_on_space", target, self)
