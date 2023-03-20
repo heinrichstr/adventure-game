4b18f4e44b40aff68ca2dcaf2b29750d4b09a9c5
Thomas Heinrichs <tom.heinri@gmail.com>
2023-03-20 14:10:21 -0400

    import scenes and assets from starter project

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,15 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	References.world = $World
+	References.game_node = self
+	$AudioStreamPlayer.playing = true
 	Input.set_custom_mouse_cursor(References.default_cursor)
 	
 	#DisplayServer.window_set_size(Vector2i(1240, 1080))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+
+func _on_menu_btn_pressed():
+	if $MainMenu.shown:
+		$MainMenu.hideMenu()
+	else:
+		$MainMenu.showMenu()
